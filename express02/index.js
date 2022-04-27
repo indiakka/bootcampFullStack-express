c0b2b554628b38cfc22ba993f0d8220fb1e63c07
indiakka <78439130+indiakka@users.noreply.github.com>
2022-04-27 17:28:19 +0200

    PUT /mascotas

--- express02/index.js
+++ express02/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const  { v4 : uuidv4 } = require ("uuid");
-const { crear, listar } = require("../data-handler");
+const { v4: uuidv4 } = require("uuid");
+const { crear, listar, actualizar } = require("../data-handler");
 const app = express();
 const port = 5000;
 
@@ -13,22 +13,38 @@ app.get("/", (req, res) => {
   res.send("La API está funcionando sin problema");
 });
 
-app.get("/mascotas", async(req, res) => {
- const mascotas = await listar({directorioEntidad: 'mascotas'})
+app.get("/mascotas", async (req, res) => {
+  const mascotas = await listar({ directorioEntidad: "mascotas" });
   res.status(200).json(mascotas);
 });
 
 app.post("/mascotas", async (req, res) => {
   if (req.body && Object.keys(req.body).length > 0) {
-      const _id = uuidv4();
-      const datosMascotaNueva ={...req.body, _id}
+    const _id = uuidv4();
+    const datosMascotaNueva = { ...req.body, _id };
     const nuevaMascota = await crear({
       directorioEntidad: "mascotas",
       nombreArchivo: _id,
       datosGuardar: datosMascotaNueva,
-      
     });
     return res.status(200).json(nuevaMascota);
+  }
+  return res.status(400).json({ mensaje: "Falta el body" });
+});
+
+app.put("/mascotas/:_id", async (req, res) => {
+  const { _id = null } = req.params;
+  if (!_id) {
+    return res.status(400).json({ mensaje: "Falta el body" });
+  }
+  if (req.body && Object.keys(req.body).length > 0) {
+       const datosActuales = { ...req.body, _id };
+      const mascotaActualizada = await actualizar( {
+      directorioEntidad: "mascotas",
+      nombreArchivo: _id,
+      datosActuales,
+    });
+    return res.status(200).json(mascotaActualizada);
   }
   return res.status(400).json({ mensaje: "Falta el body" });
 });
