a489feed051df368714d2e0fcbf937564d90632a
indiakka <78439130+indiakka@users.noreply.github.com>
2022-05-03 18:09:35 +0200

    CRUD duenos: creacion de schema, model y controller para los dueños

---
+++ express03-mongo/rutas/controllers/duenos/schema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const duenoSchema = new Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  dni: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("duenos", duenoSchema);
