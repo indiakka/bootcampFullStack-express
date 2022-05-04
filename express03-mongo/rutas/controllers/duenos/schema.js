1fb3e4c3e481725c2975c1e5a00700b562d4e9cd
indiakka <78439130+indiakka@users.noreply.github.com>
2022-05-04 16:17:38 +0200

    Timestamps: agregar fecha de creación y actualización a los modelos

--- express03-mongo/rutas/controllers/duenos/schema.js
+++ express03-mongo/rutas/controllers/duenos/schema.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const duenoSchema = new Schema({
-  nombre: {
-    type: String,
-    required: true,
+const duenoSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    apellido: {
+      type: String,
+      required: true,
+    },
+    dni: {
+      type: String,
+      required: true,
+    },
   },
-  apellido: {
-    type: String,
-    required: true,
-  },
-  dni: {
-    type: String,
-    required: true,
-  },
-});
-
+  { timestamps: true }
+);
 module.exports = mongoose.model("duenos", duenoSchema);
--- express03-mongo/rutas/controllers/veterinarias/schema.js
+++ express03-mongo/rutas/controllers/veterinarias/schema.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const veterinariaSchema = new Schema({
-  nombre: {
-    type: String,
-    required: true,
+const veterinariaSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    apellido: {
+      type: String,
+      required: true,
+    },
+    documento: {
+      type: Number,
+      required: true,
+    },
   },
-  apellido: {
-    type: String,
-    required: true,
-  },
-  documento: {
-    type: Number,
-    required: true,
-  },
-});
-
+  { timestamps: true }
+);
 module.exports = mongoose.model("veterinarias", veterinariaSchema);
