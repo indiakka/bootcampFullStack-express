c8578974c865c769f9cab018c82228ea7ca7de2d
indiakka <78439130+indiakka@users.noreply.github.com>
2022-05-05 16:30:54 +0200

    Método genérico para actualizar documentos en los controladores

--- express03-mongo/rutas/controllers/duenos/index.js
+++ express03-mongo/rutas/controllers/duenos/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { listar, obtenerUno, crear } = require("../genericos");
+const { listar, obtenerUno, crear, actualizar } = require("../genericos");
 const Dueno = require("./schema");
 
 const listarHandler = listar( {Modelo: Dueno});
@@ -11,26 +11,8 @@ router.get("/:_id", obtenerUnoHandler);
 const crearHandler = crear({ Modelo: Dueno });
 router.post("/", crearHandler);
 
-//const editarHandler = actualizar(entidad);
-router.put("/:_id", async (req, res) => {
-  try {
-    const { _id = null } = req.params;
-    const { body = {} } = req;
-    const { _id: id, ...datosNuevos } = req.body;
-    // separamos el id de los demás datos
-    if (!_id) {
-      return res.status(400).json({ mensaje: "Falta id" });
-    }
-    const duenoActualizado = await Dueno.findOneAndUpdate(
-      { _id },
-      { $set: datosNuevos },
-      { new: true, runValidators: true } // entrega los datos nuevos y verifica validaciones
-    );
-    return res.status(200).json(duenoActualizado);
-  } catch (error) {
-    return res.status(500).json({ mensaje: error.message });
-  }
-});
+const editarHandler = actualizar({ Modelo:Dueno });
+router.put("/:_id", editarHandler);
 
 //const eliminarHandler = eliminar(entidad);
 router.delete("/:_id", async (req, res) => {
