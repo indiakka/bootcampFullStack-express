771c23adea4c93e8c41c209a2bdb99297f5f2fef
indiakka <78439130+indiakka@users.noreply.github.com>
2022-05-05 18:30:11 +0200

    Corrección bug: Verificar que el dueño existe antes de crear una mascota

--- express03-mongo/rutas/controllers/mascotas/index.js
+++ express03-mongo/rutas/controllers/mascotas/index.js
@@ -1,18 +1,35 @@
 const router = require("express").Router();
-const { listar, obtenerUno, crear, actualizar, eliminar } = require("../genericos");
+const {
+  listar,
+  obtenerUno,
+  crear,
+  actualizar,
+  eliminar,
+} = require("../genericos");
 const Mascota = require("./schema");
+const Dueno = require("../duenos/schema");
 
-const listarHandler = listar({Modelo: Mascota, populate: ['dueno']});
-router.get("/", listarHandler );
+const listarHandler = listar({ Modelo: Mascota, populate: ["dueno"] });
+router.get("/", listarHandler);
 
-const obtenerUnoHandler = obtenerUno({Modelo: Mascota});
+const obtenerUnoHandler = obtenerUno({ Modelo: Mascota });
 router.get("/:_id", obtenerUnoHandler);
 
 const crearHandler = crear({ Modelo: Mascota });
-router.post("/", crearHandler);
+router.post("/", async (req, res) => {
+  const { dueno = null } = req.body;
+  const existeDueno = await Dueno.exists({ _id: dueno });
+
+  if (existeDueno) {
+    return crearHandler(req, res);
+  }
+  return res
+    .status(400)
+    .json({ mensaje: `Dueñ@ con _id ${dueno} no existe!` });
+});
 
 const editarHandler = actualizar({ Modelo: Mascota });
-router.put("/:_id", editarHandler)
+router.put("/:_id", editarHandler);
 
 const eliminarHandler = eliminar({ Modelo: Mascota });
 router.delete("/:_id", eliminarHandler);
