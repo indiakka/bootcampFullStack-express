ea5fff6455dafd6a570cbb4f4ce987f6afac14cf
indiakka <78439130+indiakka@users.noreply.github.com>
2022-05-06 13:42:49 +0200

    Remover passwords de las respuestas del servidor - parte 1

--- express04-mongo-jwt/rutas/controllers/usuarios/index.js
+++ express04-mongo-jwt/rutas/controllers/usuarios/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const Usuario = require("./schema");
+const Usuario = require( "./schema" );
+const {manejadorDeErrores}= require('../../util')
 
 const {
   listar,
@@ -19,14 +20,36 @@ router.get("/:_id", obtenerUnoHandler);
 const crearHandler = crear({ Modelo: Usuario });
 const middlewareExisteDocumento = existeDocumento({
   Modelo: Usuario,
-  campos: ["dni"],
+  campos: ["documento"],
 });
-router.post("/", middlewareExisteDocumento, crearHandler);
+router.post("/", middlewareExisteDocumento, async (req, res, next) => {
+  try {
+    if (!req.body || !Object.keys(req.body).length) {
+      const err = new createError[400]("Falta el body");
+      return next(err);
+    }
+    const { _id, ...restoDatosEntidad } = req.body;
+    let usuario = new Usuario(restoDatosEntidad);
+    await usuario.save();
+    usuario = removerPaswordDeRespuestas(usuario);
+    return res.status(200).json(usuario);
+  } catch (error) {
+    return manejadorDeErrores({ error, next });
+  }
+});
+
+const removerPaswordDeRespuestas = (objeto) => {
+  if (!(typeof objeto.toJSON === "function"))
+    throw new Error("no es instancia de mongoose");
+  objeto = objeto.toJSON();
+  const { password, ...resto } = objeto;
+  return resto;
+};
 
 const editarHandler = actualizar({ Modelo: Usuario });
 const middlewareExisteEntidadConMismoDocumentoyDiferenteId = existeDocumento({
   Modelo: Usuario,
-  campos: ["dni", { operador: "$ne", nombre: "_id" }],
+  campos: ["documento", { operador: "$ne", nombre: "_id" }],
 });
 router.put(
   "/:_id",
